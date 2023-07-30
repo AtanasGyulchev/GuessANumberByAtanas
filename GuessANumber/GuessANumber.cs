3bfdfe181cac30c6a65bfe72ccc7614cd00e03c8
AtanasGyulchev <140911944+AtanasGyulchev@users.noreply.github.com>
2023-07-30 22:15:12 +0300

    Update GuessANumber.cs

--- GuessANumber/GuessANumber.cs
+++ GuessANumber/GuessANumber.cs
@@ -49,46 +49,5 @@ while (playAgain)
 }
 
 
-
-while (playAgain)
-{
-    guess = 0;
-    guesses = 0;
-    response = "";
-    number = random.Next(min, max + 1);
-
-    while (guess != number)
-    {
-        Console.WriteLine($"Guess a number between {min} - {max}: ");
-        guess = int.Parse(Console.ReadLine());
-        Console.WriteLine($"Guess: {guess}");
-
-        if (guess > number)
-        {
-            Console.WriteLine($"{guess} is too high!");
-        }
-        else if (guess < number)
-        {
-            Console.WriteLine($"{guess} is too low!");
-        }
-
-        guesses++;
-    }
-    Console.WriteLine($"Number: {number}");
-    Console.WriteLine("YOU WIN!");
-    Console.WriteLine($"Guesses: {guesses}");
-
-    Console.WriteLine("Would you like to play again? (Y/N): ");
-    response = Console.ReadLine().ToUpper();
-
-    if (response == "Y")
-    {
-        playAgain = true;
-    }
-    else
-    {
-        playAgain = false;
-    }
-}
 Console.WriteLine("Thanks for playing!"); 
 
